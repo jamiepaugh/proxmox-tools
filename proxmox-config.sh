617ff22530e351a324fbef915634e6c5b8d2cc32
Jamie Paugh <jamiepaughdev@gmail.com>
2023-02-09 13:08:28 -0500

    Update proxmox-config.sh

--- proxmox-config.sh
+++ proxmox-config.sh
@@ -50,6 +50,7 @@ function disable-subscription-message(){
 
 function setup-fail2ban(){
 
+    apt update
     apt install fail2ban -y
     cp ./fail2ban-files/defaults-debian.conf /etc/fail2ban/jail.d/defaults-debian.conf
     cp ./fail2ban-files/pve-web-auth.conf /etc/fail2ban/jail.d/
